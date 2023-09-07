f46110eed45a3412216a926f63c6bff646958b8b
PavanS1609 <142468149+PavanS1609@users.noreply.github.com>
2023-09-07 07:59:11 +0530

    Add files via upload

---
+++ Snake&Ladder_UC2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class SnakeAndLadder_UC2
+{
+    static void Main(string[] args)
+    // UseCase2 for Snake&Ladder
+    {
+        int die;
+        Random random = new Random();
+        die = random.Next(1, 7);
+        Console.WriteLine(die);
+
+    }
+}
